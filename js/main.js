9d78bc555af2c7a5d67ace43d81cd02551b7f8b3
ChikagoDen <perepelkin-da@yandex.ru>
2021-01-26 13:41:59 +0600

    Добавьте значения по умолчанию для аргументов функции. Как можно упростить или сократить запись функций

--- js/main.js
+++ js/main.js
@@ -1,26 +1,23 @@
 const products = [
-    {id: 1, title: 'Notebook', price: 20000,  url:'img/zagl.png'},
-    {id: 2, title: 'Mouse', price: 1500,url:"img/zagl.png"},
-    {id: 3, title: 'Keyboard', price: 5000,url:"img/zagl.png"},
-    {id: 4, title: 'Gamepad', price: 4500,url:"img/zagl.png"},
+    {id: 1, title: 'Notebook', price: 20000},
+    {id: 2, title: 'Mouse', price: 1500,url:"img/mous.jpg"},
+    {id: 3, title: 'Keyboard', price: 5000,url:"img/clava.jpg"},
+    {id: 4, title: 'Gamepad', price: 4500},
 ];
+// url -параметр по умолчанию
+// убрал ретерн и {}
+const renderProduct = (title, price,url="img/zagl.png") => 
+    `<div class="product-item">
+        <img src=${url} alt="фото товара"> 
+        <h3>${title}</h3>
+        <p>${price+` &#8381`}</p>
+        <button class="by-btn">Добавить в корзину</button>
+    </div>`;
 
-const renderProduct = (title, price,url) => {
-    return `<div class="product-item">
-                <img src=${url} alt="фото товара">
-                <h3>${title}</h3>
-                <p>${price+` &#8381`}</p>
-                <button class="by-btn">Добавить в корзину</button>
-              </div>`;
-};
-
-const renderProducts = (list) => {
-    const productList = list.map(function (product) {
-        return renderProduct(product.title, product.price,product.url);
-    });
-    console.log(productList);
+// убрал ретерн, (),{}
+const renderProducts = list => {
+    const productList = list.map( product => renderProduct(product.title, product.price,product.url));
     document.querySelector('.products').innerHTML = productList;
-    // insertAdjacentHTML();
 };
 
 renderProducts(products);
