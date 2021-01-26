1acd265fc3e6e16962ae667a97bef4bab984ceb1
ChikagoDen <perepelkin-da@yandex.ru>
2021-01-26 12:24:14 +0600

    Добавьте стили для верхнего меню, товара, списка товаров и кнопки вызова корзины

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
 const products = [
-    {id: 1, title: 'Notebook', price: 20000},
-    {id: 2, title: 'Mouse', price: 1500},
-    {id: 3, title: 'Keyboard', price: 5000},
-    {id: 4, title: 'Gamepad', price: 4500},
+    {id: 1, title: 'Notebook', price: 20000,  url:'img/zagl.png'},
+    {id: 2, title: 'Mouse', price: 1500,url:"img/zagl.png"},
+    {id: 3, title: 'Keyboard', price: 5000,url:"img/zagl.png"},
+    {id: 4, title: 'Gamepad', price: 4500,url:"img/zagl.png"},
 ];
 
-const renderProduct = (title, price, img = '') => {
+const renderProduct = (title, price,url) => {
     return `<div class="product-item">
+                <img src=${url} alt="фото товара">
                 <h3>${title}</h3>
-                <p>${price}</p>
+                <p>${price+` &#8381`}</p>
                 <button class="by-btn">Добавить в корзину</button>
               </div>`;
 };
 
 const renderProducts = (list) => {
     const productList = list.map(function (product) {
-        return renderProduct(product.title, product.price);
+        return renderProduct(product.title, product.price,product.url);
     });
     console.log(productList);
     document.querySelector('.products').innerHTML = productList;
